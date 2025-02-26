6efc5a59f835f85380c0da3f544ba7ce9754b75f
David Dong <hidaviddong@gmail.com>
2025-02-26 20:56:19 +0800

    feat: point class for elliptic curve

--- index.ts
+++ index.ts
@@ -78,7 +78,70 @@ export default class FieldElement {
     return this.mul(other.pow(exponent));
   }
 }
+
 const a = new FieldElement(7, 19);
 const b = new FieldElement(5, 19);
 console.log(a.truediv(b).getNum() === 9);
 console.log(a.pow(-2).getNum());
+
+// 椭圆曲线 y^2 = x^3 + ax + b
+class Point {
+  private x: number;
+  private y: number;
+  private a: number;
+  private b: number;
+  constructor(x: number, y: number, a: number, b: number) {
+    this.x = x;
+    this.y = y;
+    this.a = a;
+    this.b = b;
+    if (this.y ** 2 !== this.x ** 3 + a * x + b) {
+      throw new Error(`(${x}, ${y}) is not on the curve`);
+    }
+  }
+  eq(other: Point) {
+    return (
+      this.x === other.x &&
+      this.y === other.y &&
+      this.a === other.a &&
+      this.b === other.b
+    );
+  }
+  neq(other: Point) {
+    return !this.eq(other);
+  }
+  add(other: Point) {
+    if (other.a !== this.a || other.b !== this.b) {
+      throw new Error(`Points ${this}, ${other} are not on the same curve`);
+    }
+    if (!isFinite(other.x) || !isFinite(other.y)) {
+      return new Point(this.x, this.y, this.a, this.b);
+    }
+
+    if (this.x + other.x === 0 && this.y + other.y === 0) {
+      return new Point(Infinity, Infinity, this.a, this.b);
+    }
+    if (this.eq(other)) {
+      // 因为P1 = P2,所以需要通过求导来得到斜率。 P1 = (x1,y1), P3 = (x3,y3), P1 + P1 = P3, s = (3x1^2 + a)/(2y1), x3 = s^2 – 2x1, y3 = s(x1 – x3) – y1
+      const slope = (3 * this.x ** 2 + this.a) / (2 * this.y);
+      const x3 = slope ** 2 - 2 * this.x;
+      const y3 = slope * (this.x - x3) - this.y;
+      return new Point(x3, y3, this.a, this.b);
+    } else {
+      // P1 != P2,可以直接计算得到斜率: P1 = (x1,y1), P2 = (x2,y2), P3 = (x3,y3); P1 + P2 = P3, s = (y2 – y1)/(x2 – x1); x3 = s^2– x1 – x2, y3 = s(x1 – x3) – y1
+      if (this.y === 0) {
+        return new Point(Infinity, Infinity, this.a, this.b);
+      }
+      const slope = (other.y - this.y) / (other.x - this.x);
+      const x3 = slope ** 2 - this.x - other.x;
+      const y3 = slope * (this.x - x3) - this.y;
+      return new Point(x3, y3, this.a, this.b);
+    }
+  }
+}
+// 3/6 = 0.5
+const p1 = new Point(2, 5, 5, 7);
+const p2 = new Point(-1, -1, 5, 7);
+const p3 = new Point(-1, 1, 5, 7);
+console.log(p1.add(p2)); // P1 != P2
+console.log(p3.add(p3)); // P1 = P2
